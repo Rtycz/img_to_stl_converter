9b1b9e3bf74c471d1313e28aa635ff9e553808cd
Kurdyukov Anton Gennadyevich <akurdyukov@ozon.ru>
2025-03-03 21:56:28 +0300

    [CONV-17] optimization

--- static/js/script.js
+++ static/js/script.js
@@ -57,6 +57,20 @@ window.addEventListener('beforeunload', function(event) {
     }
 });
 
+function openTab(evt, tabName) {
+    var i, tabContent, tabButtons;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabButtons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 function updateProcessedImage(filename) {
     const maxValue = document.getElementById('maxValue').value;
     const adaptiveMethod = document.getElementById('adaptiveMethod').value;
